a6317cd815a0f403c146bc0cdca7538ffc0f4206
Dhruv <dhruvkandpal02@gmail.com>
2024-07-14 22:39:26 +0530

    initial commit

---
+++ Day 1/day1.js
@@ -0,0 +1,45 @@
+// Activity 1
+var num = 1;
+console.log(num);
+
+let word = "30 Day Challenge";
+console.log(word);
+
+// Activity 2
+const checker = true;
+console.log(checker);
+
+// Activity 3
+let number = 5;
+console.log(typeof(number));
+
+let string = "New string";
+console.log(typeof(string));
+
+let bool = false;
+console.log(typeof(bool));
+
+let newArr = [1, 2, 3];
+console.log(typeof(newArr));
+
+let newObj = {
+    day: 1,
+    language: "javacript"
+}
+console.log(typeof(newObj));
+
+
+// Activity 4
+let initial = 20;
+console.log(initial);
+initial = 30;
+console.log(initial);
+
+
+// Activity 5
+const newInitial = 10;
+console.log(newInitial);
+newInitial = 20;
+console.log(newInitial);
+// Error on reassigning
+// TypeError: Assignment to constant variable.
